53032608c985290d171f806e6e93f222ea70479e
mortalreflections <sivamsrini214@gmail.com>
2023-05-04 20:09:24 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ElectricityConsumer ec = new ElectricityConsumer();
+        ec.displayDomesticFares_BiMonth_Monthly();
+        ec.getDetails();
+        ec.displayBill();
+    }
+
+}
